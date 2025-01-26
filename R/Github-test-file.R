0313ce829f6016280e6f276c9af25d2df3eb6c3b
WyattEich <30613006+wyatteich@users.noreply.github.com>
2025-01-25 19:40:41 -0600

    Okay, this is kinda fun. I think I could get the hang of this.

--- R/Github-test-file.R
+++ R/Github-test-file.R
@@ -2,3 +2,5 @@
 #'
 #' It may be more viable, in the future, to do this from a VSCode terminal.
 #' But I would really love for it to work naturally in the IDE.
+#'
+#' It worked! Okay, I know that I can mess around with this.

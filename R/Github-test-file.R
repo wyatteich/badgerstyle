5fd76ac07e5d2e2f555da5a5e413161f6953a054
WyattEich <30613006+wyatteich@users.noreply.github.com>
2025-01-25 19:48:10 -0600

    More additions to the github file

--- R/Github-test-file.R
+++ R/Github-test-file.R
@@ -4,7 +4,27 @@
 #' But I would really love for it to work naturally in the IDE.
 #'
 #' It worked! Okay, I know that I can mess around with this.
-#' 
+#'
 #' Okay, even cooler, I can go back and forth and do this in my
-#' other repo open in VSCode. It's a bit janky, I know. But 
-#' I can work with this. 
+#' other repo open in VSCode. It's a bit janky, I know. But
+#' I can work with this.
+#'
+#' Now I am starting to see the wisdom in what Kepano once said in one of his essays:
+#' plain text is king; all organizations should be using github for all files.
+#'
+#' Imagine how much simpler it would make the editing process for my work if we
+#' all went back and forth in git. Every change would be accounted for.
+#' All files available for everyone to read at any time and collaborate on.
+#'
+#' But we probably won't see that at Badger anytime soon. And to be honest,
+#' the collaborative features of Google drive or Microsoft One Drive probably do the trick.
+#'
+#' Now that I've mastered this, I should try to use it more often.
+#'
+#' Maybe I make a BTN Repo, and all of my files go there?
+#'
+#' (Here's a question...do I dare try setting this up between my onedrive? No,
+#' I bet that would be awful. I can see a lot of ways that could go wrong.)
+#'
+#' I will just say this: having all of my important work living in github would
+#' make it a lot easier to collaborate between my macbook and my

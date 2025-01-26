0c078b5d7bf18b9625173932e5d3c80ed0c2cbf7
WyattEich <30613006+wyatteich@users.noreply.github.com>
2025-01-25 19:39:21 -0600

    Testing uplaoding a new file

---
+++ R/Github-test-file.R
@@ -0,0 +1,4 @@
+#' this is a test of the github functionality in R. Studio.
+#'
+#' It may be more viable, in the future, to do this from a VSCode terminal.
+#' But I would really love for it to work naturally in the IDE.

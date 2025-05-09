b54d633333ddf1365ed44a0561d246b54b66b5d3
WyattEich <30613006+wyatteich@users.noreply.github.com>
2025-05-09 11:23:25 -0500

    Programmatically generate all lines (with endpoints)

--- R/badger_line.R
+++ R/badger_line.R
@@ -5,31 +5,38 @@
 #' @param data the data frame that the plot is based on
 #' @param x the variable for the x axis
 #' @param y the variable for the y axis
-#' @param color the color of the line and endpoints
+#' @param color_scale the column that defines the color scale
+#' @param width the width of the desired lines
 
-badger_line <- function(data, x, y, color) {
-
+badger_line <- function(data, x, y, color_scale, width = 4) {
   endpoints <- dplyr::bind_rows(
     dplyr::filter(data, {{x}} == max({{x}})),
     dplyr::filter(data, {{x}} == min({{x}}))
   )
 
-  list(
-    geom_line(data = data,
-              aes(x = {{x}}, y = {{y}}),
-              color = color,
-              linewidth = 4,
-              lineend = "round"),
-    geom_point(data = endpoints,
-               aes(x = {{x}}, y = {{y}}),
-               color = color,
-               size= 8),
-    geom_point(data = endpoints,
-               aes(x = {{x}}, y = {{y}}),
-               color = "white",
-               size = 3)
-  )
+  lines <- levels(data[[color_scale]])
 
+  line_functions <- list()
 
+  for(i in 1:length(lines)) {
+    line_functions <- list(
+      geom_line(
+        data = data,
+        aes(x = {{x}}, y = {{y}}),
+        linewidth = width,
+        lineend = "round"
+      ),
+
+      geom_point(
+        data = endpoints,
+        aes(x = {{x}}, y = {{y}}),
+        shape = 21,
+        stroke = width,
+        size = width * 0.925,
+        fill = "white",
+      )
+    )
+  }
+
+  line_functions
 }
-
